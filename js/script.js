b8fa2845beb0a991f23aa3da14f55fbde2d934aa
rbaggio24 <robert.horvath.24@gmail.com>
2020-06-09 18:48:34 +0200

    takeaway

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
 function calcAmount() {
-    const name = document.querySelector('#name').value.trim();
+    let price = 1900;
+    let amountNumber = parseInt(document.querySelector("input[name='amount-input']").value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    showSumPrice(price, amountNumber);
+    validateData(amountNumber);
+};
+
+function showSumPrice(price, amountNumber) {
+    let extra = document.querySelector('input[type="radio"]:checked');
+    document.querySelector("span.show-amount").innerHTML = addTakeAway(parseInt(amountNumber) * (price + parseInt(extra.value) + parseInt(sauce.value)));
+};
+
+function addTakeAway(total) {
+    return (total < 5000) ? total += 500 : total;
+};
+
+function validateData(amountNumber) {
+    const name = document.querySelector("input[name='name']").value.trim();
     const email = document.querySelector('#email').value.trim();
     const address = document.querySelector('#address').value.trim();
     const comment = document.querySelector('#comment').value.trim();
-    let price = 1200;
-    let extra = document.querySelector('input[type="radio"]:checked');
-    let sauce = document.querySelector('#sauce');
-    let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
-    let amountNumber = parseInt(amountInput.value);
-
-    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
-
-    if (amountNumber > 10) {
+    if (!name) {
+        alert("A név megadása kötelező");
+    } else if ((!email) || (email.indexOf('@') < 0)) {
+        alert("Invalid email");
+    } else if (address.length < 10) {
+        alert("írj már be egy normális címet");
+    } else if (amountNumber > 10) {
         alert("Maximum 10 terméket vásárolhat!");
     } else if (amountNumber < 1) {
         alert("Na ne szórakozz!");
-    } else if (!name) {
-        alert("A név megadása kötelező");
     } else {
         let message = document.querySelector("#message");
-        let amount = parseInt(amountInput.value) * price + parseInt(extra.value) * parseInt(amountInput.value) + parseInt(sauce.value) * parseInt(amountInput.value);
-        showAmount.innerHTML = amount;
+        return message;
     }
-}
+};
